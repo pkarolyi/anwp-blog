4984964818241473a3e7485d4180682d951bf627
Peter Karolyi <peter@karolyi.dev>
2024-02-13 19:04:23 +0100

    chore: update packages

--- app/cover-image/route.tsx
+++ app/cover-image/route.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import { ImageResponse } from "next/server";
+import { ImageResponse } from "next/og";
 
 const size = {
   width: 1200,
